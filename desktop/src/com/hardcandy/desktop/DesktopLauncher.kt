18d7ae3f505af5727d968f75e5a8435f9fb0238f
Steve Holmes <iamsteveholmes@gmail.com>
2018-06-23 17:39:56 -0700

    initial commit

---
+++ desktop/src/com/hardcandy/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.hardcandy.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.hardcandy.Game
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
